b85248c56b40bedeb75e4ef5b27f742e70fb7f9d
alifarazz <alifarazdaghi@gmail.com>
2021-12-21 21:27:19 +0330

    Adjust ProgrssBar's ticking duration

--- src/utils/progress_bar.hh
+++ src/utils/progress_bar.hh
@@ -2,35 +2,40 @@
 
 #include <iostream>
 #include <string_view>
-namespace partou
-{
-namespace cli
+namespace partou::cli
 {
 class ProgressBar
 {
   const std::size_t total;
-  std::string msg_view;
-  std::ostream& out;
-  std::size_t idx;
+  std::string_view m_msg_view;
+  const std::size_t m_tick_duration;
+  std::ostream& m_out;
+  std::size_t m_idx;
+  std::size_t m_tick_duration_counter {0};
 
 public:
-  ProgressBar(std::size_t total,
-              std::string_view msg_view = std::string_view {""},
-              std::ostream& out = std::cerr,
-              std::size_t idx = 0)
-      : total {total}
-      , msg_view {msg_view}
-      , out {out}
-      , idx {idx}
+  explicit ProgressBar(std::size_t total,
+                       std::string_view msg_view = std::string_view {""},
+                       std::size_t tick_duration_shift = 0,
+                       std::ostream& out = std::cerr,
+                       std::size_t idx = 0)
+      : total {total >> tick_duration_shift}
+      , m_msg_view {msg_view}
+      , m_tick_duration {std::size_t(tick_duration_shift > 0 ? 1 : 0) << tick_duration_shift}
+      , m_out {out}
+      , m_idx {idx}
   {
   }
 
   void tick()
   {
-    const auto percent = 100.f * (static_cast<float>(idx++) / total);
-    out << '\r' << msg_view << ": " << static_cast<int>(percent) << "%"
-        << std::flush;
+    if (m_tick_duration_counter == 0) {
+      const auto percent = 100.F * (static_cast<float>(m_idx++) / total);
+      m_out << '\r' << m_msg_view << ": " << static_cast<int>(percent) << "%" << std::flush;
+      m_tick_duration_counter = m_tick_duration;
+    } else {
+      m_tick_duration_counter--;
+    }
   }
 };
-}  // namespace cli
-}  // namespace partou
+}  // namespace partou::cli
